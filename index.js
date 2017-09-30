aa0ed60d3028a65d67aa35a574ff9b591b7b7f82
ronniekinsley <harundilshad@gmail.com>
2017-09-30 16:06:41 +0500

    first working watcher

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ Special thanks to https://violentatom.com/2015/07/08/node-js-chokidar-wait-for-f
 // Setup video source folder observer for notifications of new files
 var chokidar = require('chokidar');
 var logger = require('logger').createLogger(); // logs to STDOUT
-
-var watcher = chokidar.watch('~/Downloads', {
+var fs = require('fs');
+var WAITSECONDS = 1;
+var watcher = chokidar.watch(['/Users/ronniekinsley/Downloads/Test'], {
     persistent: true,
     followSymlinks: false,
     usePolling: true,
     depth: undefined,
     interval: 100,
-    ignorePermissionErrors: false
+    ignorePermissionErrors: false,
+    ignoreInitial: true
 });
 
 watcher
     .on('ready', function() { logger.info('Initial scan complete. Ready for changes.'); })
     .on('unlink', function(path) { logger.info('File: ' + path + ', has been REMOVED'); })
     .on('error', function(err) {
-        logger.error(component + 'Chokidar file watcher failed. ERR: ' + err.message);
+        logger.error('Chokidar file watcher failed. ERR: ' + err.message);
     })
     .on('add', function(path) {
         logger.info('File', path, 'has been ADDED');
@@ -26,11 +28,11 @@ watcher
         fs.stat(path, function (err, stat) {
 
             if (err){
-                logger.error(component + 'Error watching file for copy completion. ERR: ' + err.message);
-                logger.error(component + 'Error file not processed. PATH: ' + path);
+                logger.error('Error watching file for copy completion. ERR: ' + err.message);
+                logger.error('Error file not processed. PATH: ' + path);
             } else {
-                logger.info(component + 'File copy started...');
-                setTimeout(checkFileCopyComplete, fileCopyDelaySeconds*1000, path, stat);
+                logger.info('File copy started...');
+                setTimeout(checkFileCopyComplete, WAITSECONDS*1000, path, stat);
             }
         });
     });
@@ -44,13 +46,13 @@ function checkFileCopyComplete(path, prev) {
             throw err;
         }
         if (stat.mtime.getTime() === prev.mtime.getTime()) {
-            logger.info(component + 'File copy complete => beginning processing');
+            logger.info('File copy complete => beginning processing');
             //-------------------------------------
             // CALL A FUNCTION TO PROCESS FILE HERE
             //-------------------------------------
         }
         else {
-            setTimeout(checkFileCopyComplete, fileCopyDelaySeconds*1000, path, stat);
+            setTimeout(checkFileCopyComplete, WAITSECONDS*1000, path, stat);
         }
     });
 }
